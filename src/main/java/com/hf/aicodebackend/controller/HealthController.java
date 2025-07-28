d6c2af019e316843b4b71a458b3d6a092586a35c
HF <3123961845@qq.com>
2025-07-28 17:43:13 +0800

    初始化

---
+++ src/main/java/com/hf/aicodebackend/controller/HealthController.java
@@ -0,0 +1,18 @@
+package com.hf.aicodebackend.controller;
+
+import com.hf.aicodebackend.common.BaseResponse;
+import com.hf.aicodebackend.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping("/")
+    public BaseResponse<String> healthCheck() {
+        return ResultUtils.success( "ok");
+    }
+}
+
---
+++ src/test/java/com/hf/aicodebackend/AiCodeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hf.aicodebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCodeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
