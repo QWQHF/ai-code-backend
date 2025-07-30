7bde533edd35fef28824a0af7bf89f2d749db2b7
HF <3123961845@qq.com>
2025-07-30 21:58:49 +0800

    用户模块

---
+++ src/main/java/com/hf/aicodebackend/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.hf.aicodebackend.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.hf.aicodebackend.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author <a href="https://github.com/QWQHF">HF</a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
