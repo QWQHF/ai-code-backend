4ec539cbeeb192a9eb405b6f36bbde3fa2b82817
HF <3123961845@qq.com>
2025-07-30 21:58:02 +0800

    用户模块

--- src/main/java/com/hf/aicodebackend/AiCodeBackendApplication.java
+++ src/main/java/com/hf/aicodebackend/AiCodeBackendApplication.java
@@ -1,8 +1,10 @@
 package com.hf.aicodebackend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.hf.aicodebackend.mapper")
 @SpringBootApplication
 public class AiCodeBackendApplication {
 
